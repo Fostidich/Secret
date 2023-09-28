9dd21f7b548ce1290c7c1f1815c3a48e5df86d35
Fostidich <francesco.ostidich@mail.polimi.it>
2023-09-29 01:12:25 +0200

    values and block fn done

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,63 @@ fn main() {
     let username : Vec<char> = args[2].chars().collect();
     let key : Vec<char> = args[3].chars().collect();
 
-    for ch in website {
-        println!("{}", get_value(ch));
+    let mut first_block : Vec<u8> = get_block(&website, &key);
+    let mut second_block : Vec<u8> = get_block(&username, &key);
+
+    for ch in key {
+        swap_char(&mut first_block, &mut second_block, &ch);
+        shift_rows(&mut first_block, &mut second_block, &ch);
+        shift_columns(&mut first_block, &mut second_block, &ch);
+        combine_key(&mut first_block, &mut second_block, &ch);
     }
+
+    
+}
+
+fn get_value(ch: &char) -> u8 {
+    return match ch {
+        'A'..='Z' => ch as u8 - 29,
+        'a'..='z' => ch as u8 - 87,
+        '0'..='9' => ch as u8 - 48,
+        '.' => 62,
+        _ => 63,
+    }
+}
+
+fn get_block(chars: &[char], key: &[char]) -> Vec<u8> {
+    let result : Vec<u8> = vec![0; 8];
+    let mut i : u8 = 0;
+    for j in 1..8 {
+        if get_value(&key[j]) < 32 {
+            i += 1
+        }
+    }
+    for ch in chars {
+        result[i] += get_value(ch);
+        if i != 7 {
+            i += 1
+        } else {
+            i = 0
+        }
+    }
+    for i in 0..8 {
+        result[i] = result[i]%64
+    }
+    return result;
+}
+
+fn swap_char(first_block: &mut Vec<u8>, second_block: &mut Vec<u8>, ch: &char) {
 
 }
 
-fn get_value(ch: char) -> i16 {
-    return match ch {
-        'A'..='Z' => ch as i16 - 59,
-        'a'..='z' => ch as i16 - 128,
-        '0'..='4' => ch as i16 - 53,
-        '5'..='9' => ch as i16 - 52,
-        _ => 0
-    }
+fn shift_rows(first_block: &mut Vec<u8>, second_block: &mut Vec<u8>, ch: &char) {
+
+}
+
+fn shift_columns(first_block: &mut Vec<u8>, second_block: &mut Vec<u8>, ch: &char) {
+
+}
+
+fn combine_key(first_block: &mut Vec<u8>, second_block: &mut Vec<u8>, ch: &char) {
+
 }
