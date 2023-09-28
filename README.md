24770102bbaf52c405dce4c34cf6612464fb8230
Fostidich <francesco.ostidich@mail.polimi.it>
2023-09-28 23:35:03 +0200

    input and values managed

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 # Secret - SCRT
 A command line password manager.
 
-scrt -help<br>
-scrt -get entryName username key<br>
-scrt -add entryName username password key<br>
-scrt -replace entryName username password key<br>
-scrt -remove entryName username password key<br>
-scrt -show<br>
-scrt -destroy<br>
-
-Given the name of the entry (generally, the website that we're trying to log in), the username (the email when possible), and a key that has to be remembered, a code in generated and it can be used to make a simple password securer. 
+> ##### Usage:
+> scrt *website username key*<br>
+> aBcD-0123
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
 fn main() {
-    println!("Hello");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        println!("USAGE: scrt [website] [username] [key]");
+        return
+    }
+    let website : Vec<char> = args[1].chars().collect();
+    let username : Vec<char> = args[2].chars().collect();
+    let key : Vec<char> = args[3].chars().collect();
+
+    for ch in website {
+        println!("{}", get_value(ch));
+    }
+
+}
+
+fn get_value(ch: char) -> i16 {
+    return match ch {
+        'A'..='Z' => ch as i16 - 59,
+        'a'..='z' => ch as i16 - 128,
+        '0'..='4' => ch as i16 - 53,
+        '5'..='9' => ch as i16 - 52,
+        _ => 0
+    }
 }
